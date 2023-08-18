1809aae4aaa2d9425e711e53513f9d0bd6efbde3
seugn <seugn@DESKTOP-EEBOCT7>
2023-08-18 11:05:45 +0900

    초기 설정 (인코딩추가)

--- ShoppingMall/src/main/java/com/shop/controller/HomeController.java
+++ ShoppingMall/src/main/java/com/shop/controller/HomeController.java
@@ -4,35 +4,35 @@ import java.text.DateFormat;
 import java.util.Date;
 import java.util.Locale;
 
+import javax.servlet.http.HttpServletRequest;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import lombok.extern.slf4j.Slf4j;
 
 /**
  * Handles requests for the application home page.
  */
+@Slf4j
 @Controller
 public class HomeController {
 	
-	private static final Logger logger = LoggerFactory.getLogger(HomeController.class);
 	
-	/**
-	 * Simply selects the home view to render by returning its name.
-	 */
-	@RequestMapping(value = "/", method = RequestMethod.GET)
-	public String home(Locale locale, Model model) {
-		logger.info("Welcome home! The client locale is {}.", locale);
-		
+	@GetMapping("/")
+	public String home(Locale locale, Model model, HttpServletRequest req) {
+		String ipAddress = req.getRemoteAddr();
+		log.info("HomeController Start - ＃{MappingValue = (\"/\")}＃ -- ip : " + ipAddress);
 		Date date = new Date();
 		DateFormat dateFormat = DateFormat.getDateTimeInstance(DateFormat.LONG, DateFormat.LONG, locale);
 		
 		String formattedDate = dateFormat.format(date);
 		
 		model.addAttribute("serverTime", formattedDate );
-		
+		log.info("HomeController End - ＃{MappingValue = (\"/\")}＃ -- ip : " + ipAddress);
 		return "home";
 	}
 	
